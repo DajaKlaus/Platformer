0fdf1bf2ba12a458cdb1720b2107fe1ed75c5b28
DajaKlaus <91118237+DajaKlaus@users.noreply.github.com>
2022-03-16 14:53:02 +0100

    Main && initContent()

--- src/main/java/com/example/platformer/HelloApplication.java
+++ src/main/java/com/example/platformer/HelloApplication.java
@@ -1,23 +1,102 @@
 package com.example.platformer;
 
+import javafx.animation.Animation;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.EventListener;
+import java.util.HashMap;
 
 public class HelloApplication extends Application {
+    HashMap<KeyCode, Boolean> keys = new HashMap<KeyCode, Boolean>();
+
+    ArrayList<Node> platforms = new ArrayList<Node>();
+
+    private Pane appRoot = new Pane();
+    private Pane gameRoot = new Pane();
+    private Pane uiRoot = new Pane();
+
+    private Node player;
+    private Point2D playerVelocity = new Point2D(0, 0);
+    private boolean canJump = true;
+
+    private int levelWidth;
+
+    private void initContent() {
+        Rectangle bg = new Rectangle(1280, 720);
+
+        levelWidth = LevelData.LEVEL1[0].length() * 60;
+
+        for (int i = 0; i < LevelData.LEVEL1.length; i++) {
+            String line = LevelData.LEVEL1[i];
+            for (int j = 0; j < line.length(); j++) {
+                switch (line.charAt(j)) {
+                    case '0':
+                        break;
+                    case '1':
+                        Node platform = createEntity(j * 60, i * 60, 60, 60, Color.BROWN);
+                        platforms.add(platform);
+                        break;
+                }
+            }
+        }
+        player = createEntity(0, 600, 40, 40, Color.BLUE);
+
+        player.translateXProperty().addListener((obs, old, newValue) -> {
+            int offset = newValue.intValue();
+
+            if(offset > 640 && offset < levelWidth - 640) {
+                gameRoot.setLayoutX(-(offset - 640));
+            }
+        });
+        appRoot.getChildren().addAll(bg, gameRoot, uiRoot);
+    }
+
+    private void update() {}
+
+    private void movePlayerX(int value) {}
+
+    private void movePlayerY(int value) {}
+
+    private void jumpPlayer() {}
+
+    private Node createEntity(int x, int y, int w, int h, Color color) {}
+
+    private boolean isPressed(KeyCode key) {}
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        initContent();
+
+        Scene scene = new Scene(appRoot);
+        scene.setOnKeyPressed(event -> keys.put(event.getCode(), true));
+        scene.setOnKeyReleased(event -> keys.put(event.getCode(), false));
+        primaryStage.setTitle("Platformer");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                update();
+            }
+        };
+        timer.start();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
